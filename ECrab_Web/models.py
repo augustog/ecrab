441cf58ccf2a90477e05a8a34c855b1c4b50ceee
Bruno <thebrunosmail@gmail.com>
2016-10-02 15:27:33 -0300

    Configuramos 'models'

---
+++ ECrab_Web/models.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Usuario(models.Model):
+	name = models.TextField(max_length=400)
+	password = models.TextField(max_length=300)
+
---
+++ ECrab_Web/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def login(request):
+	pass 
+
+def listas (request):
+	pass
+
+def perfil(request):
+	pass
