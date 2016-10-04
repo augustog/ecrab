1400536309f0b2fcf420886e743693ed0373f9fd
Ariel <arielviscomi.mail@gmail.com>
2016-10-04 09:47:40 -0300

    Cambio definitivo a 'models' (la habia cagado, por eso)

--- ECrab_Web/models.py
+++ ECrab_Web/models.py
@@ -5,5 +5,6 @@ from django.db import models
 # Create your models here.
 class Usuario(models.Model):
 	name = models.TextField(max_length=400)
+	lastname = models.TextField(max_length=400)
+	username = models.TextField(max_length=400) 
 	password = models.TextField(max_length=300)
-
