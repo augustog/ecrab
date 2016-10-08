d4f68b9d5c989582ae0ab257e1d7e089f136ae6e
Ariel <arielviscomi.mail@gmail.com>
2016-10-08 15:49:23 -0300

    Agregue ejemplo de 'Log in' en Views, estoy en espera de avance de Bruno

--- ECrab_Web/views.py
+++ ECrab_Web/views.py
@@ -10,6 +10,25 @@ from django.shortcuts import render
 #def perfil(request):
 #	pass
 
+"""def login_user(request):
+    logout(request)
+    username = password = ''
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/main/')
+    return render_to_response('login.html', context_instance=RequestContext(request))
+
+@login_required(login_url='/login/')
+def main(request):
+    ...."""
+
 def login (request):
 
 	return render(request, 'weblog.html')
+
