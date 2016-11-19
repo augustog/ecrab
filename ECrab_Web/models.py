be4fd39d1e031bddfd28d700fa8a27cea88fef94
Ariel <arielviscomi.mail@gmail.com>
2016-11-19 09:46:43 -0300

    Modelo 'Proyectos' añadido

--- ECrab_Web/models.py
+++ ECrab_Web/models.py
@@ -6,9 +6,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-#### ARI: Fijate toda la data que podés agregar al modelo de user uqe viene predefinido,
-#### cualquier dato que no puedas sacar de ahí, lo agregás a esta clase que te ponog acá
-#### después te muestro como se usa bien. --A
+# ARI: Fijate toda la data que podés agregar al modelo de user uqe viene predefinido,
+# cualquier dato que no puedas sacar de ahí, lo agregás a esta clase que te ponog acá
+# después te muestro como se usa bien. --A
 
 class ExtendedUser (models.Model):
 	'''
@@ -25,3 +25,10 @@ class Recordatorio (models.Model):
 
 ## Pensá qué otras cosas vas a estar manejando con la app, para eso hay que pensar el diagrama de clases.
 ## Cuando estés para avanzar avisame y lo vemos. -- A
+
+class Proyectos (models.Model):
+	name = models.textfield(max_length = 300) #Nombre del Proyecto
+	user = models.ForeignKey(User) #Usuario que lo creo
+	initdate = models.DateField() #Fecha en la que se inicia el proyecto
+	findate = models.DateField() #Fecha en la que finaliza
+	#file = ... #El archivo del proyecto
