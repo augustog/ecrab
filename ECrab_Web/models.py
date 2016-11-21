1656353c205ab6b5ef4fbc75f91a026e2745752c
Ariel <arielviscomi.mail@gmail.com>
2016-11-21 11:42:41 -0300

    commit

--- ECrab_Web/models.py
+++ ECrab_Web/models.py
@@ -6,28 +6,20 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-# ARI: Fijate toda la data que podés agregar al modelo de user uqe viene predefinido,
-# cualquier dato que no puedas sacar de ahí, lo agregás a esta clase que te ponog acá
-# después te muestro como se usa bien. --A
-
 class ExtendedUser (models.Model):
-	'''
-	Extended user class,
-	este es un approach medio choto, la otra sería heredar de User y extender a voluntad, pero esto es más fácil 
-	'''
+	
+	#Extended user class 
+	
 	user = models.ForeignKey(User)
 	#dato = models.XXField()
 
 class Recordatorio (models.Model):
     user = models.ForeignKey(User) #Check use of ForeignKey
     date = models.DateField() #Check attributes
-	done = models.BooleanField() #Check
-
-## Pensá qué otras cosas vas a estar manejando con la app, para eso hay que pensar el diagrama de clases.
-## Cuando estés para avanzar avisame y lo vemos. -- A
+    done = models.BooleanField() #Check
 
 class Proyectos (models.Model):
-	name = models.textfield(max_length = 300) #Nombre del Proyecto
+	name = models.TextField(max_length = 300) #Nombre del Proyecto
 	user = models.ForeignKey(User) #Usuario que lo creo
 	initdate = models.DateField() #Fecha en la que se inicia el proyecto
 	findate = models.DateField() #Fecha en la que finaliza
